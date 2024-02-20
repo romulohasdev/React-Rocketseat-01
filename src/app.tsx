247390fdadf5590ce25a8690fc7a262fe64e58b5
Rômulo Henrique <henriqueromulo1997@gmail.com>
2024-02-19 22:33:18 -0300

    [01] Criando arquivo aula 1:17

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        header
+        tabs
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <p>olá</p>
+        <p>mundo</p>
+      </main>
+    </div>
+
+  )
+}
